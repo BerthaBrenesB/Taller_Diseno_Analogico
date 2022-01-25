104078bafb6c14df747db572374fe2014960d27c
Abigail91 <abigail_abarca@hotmail.com>
2022-01-24 21:33:38 -0600

    Add Script

--- Proyecto 3/curvas.m
+++ Proyecto 3/curvas.m
@@ -2,29 +2,28 @@ clc
 clear
 pkg load signal
 sf = 60e3 ; sf2 = sf / 2;
-fc_1 = 30;
+fc_1 = 50;
 fc_2 = 3e3 ;
+fid = fopen('Binario.m');
+tline = fgetl(fid);
+vect = [];
+n = 1;
+while ischar(tline)
+    vect(n) = bin2dec(tline) / 1677721.6;
+    n = n+1;
+    tline = fgetl(fid);
+end
+fclose(fid);
 data=[[1;zeros(sf - 1,1)],sinetone(fc_1 , sf ,1 ,1) + sinetone(fc_2 ,sf , 1,1)];
 [b_1,a_1 ]= butter ( 1 , fc_1 / sf2);
 filtered_1 = filter ( b_1 , a_1 , data ) ;
 [ b_2 , a_2 ]= butter ( 1 , fc_2 / sf2 , "high" );
 filtered_2 = filter ( b_2 , a_2 , data ) ;
 data =[ sinetone( fc_1 , sf ,1 ,1 ) + sinetone( fc_2 , sf ,1 ,1 ) ] * 2.5 / 2 ;
-fid = fopen ("Binario.txt");
-% save full lines into a new file if only chars inside
-txtread = fgetl (fid)
-num_of_lines = fskipl(fid, Inf);
-char = 'crossplane-x-'
-for i=1:num_of_lines,
-  if char in fgetl(fid)
-    [x, nx] = fscanf(fid);
-    print x
-  endif
-endfor
-fclose (fid);
+
 clf
 subplot ( 4 , 1 , 1 )
-plot ( data , "; Entrada ; " )
+plot ( vect , "; Entrada ; " )
 axis ( [ 0 fc_1* 200] )
 subplot ( 4 , 1 , 2 )
 plot ( filtered_1 ( : , 2 ) , "; F i l t r o pasa b a j a s ; " )
